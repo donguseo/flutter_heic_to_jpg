e630816503afab7737797920637fe840aa65a2a1
Leo <dev.rignaneseleo@gmail.com>
2020-08-01 16:06:41 +0200

    Output filename matches the input one
    In this way it is easier to recognize the output.

--- android/src/main/kotlin/seo/dongu/heic_to_jpg/HeicToJpeg.kt
+++ android/src/main/kotlin/seo/dongu/heic_to_jpg/HeicToJpeg.kt
@@ -11,8 +11,14 @@ fun convertHeicToJpeg(heic: String, cacheDir: File?) : String? {
         return null
     }
     try {
+        //val fullPath = heic
+	    //val directory = fullPath.substringBeforeLast("/")
+	    val fullName = heic.substringAfterLast("/")
+	    val fileName = fullName.substringBeforeLast(".")
+	    //val extension = fullName.substringAfterLast(".")
+        
         val bitmap = BitmapFactory.decodeFile(heic)
-        val file = File(cacheDir.path + "/${System.currentTimeMillis()}.jpg")
+        val file = File(cacheDir.path + "/${fileName}.jpg")
         file.createNewFile()
         bitmap.compress(Bitmap.CompressFormat.JPEG, 100, FileOutputStream(file));
         return file.path
